4dbb5f3167050d61bce02c4bc42f49d7190cba30
fbzr <fabricio.spb@gmail.com>
2020-01-28 11:55:16 -0800

    STRETCH - processDuplicateFree

--- index.js
+++ index.js
@@ -156,8 +156,8 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  return callback(list.filter((current,index) => list.indexOf(current) === index));
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
